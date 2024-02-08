76f0e8d491a68b527b60d57e93432ce2f8ce64ae
Denis Perov <denis.perov@gmail.com>
2024-02-08 15:29:41 +0300

    Support fetching Token balance

--- app/client/ui/campaigns/funding.js
+++ app/client/ui/campaigns/funding.js
@@ -1,10 +1,15 @@
+import ethers from 'ethers';
+import { Meteor } from 'meteor/meteor';
 import { TemplateController } from 'meteor/space:template-controller';
 import { FlowRouter } from 'meteor/ostrio:flow-router-extra';
 import { Campaigns } from '/api/campaigns';
-import { Accounts } from '/api/ethers/client';
+import { Accounts, TokenContract } from '/api/ethers/client';
 import './funding.html';
 
 TemplateController('CampaignFunding', {
+  state: {
+    balance: null,
+  },
   helpers: {
     campaign() {
       return this.campaign();
@@ -12,7 +17,19 @@ TemplateController('CampaignFunding', {
   },
   events: {
     async 'click [data-action=connect]'() {
-      await Accounts.connect();
+      const account = await Accounts.connect();
+      const balance = await TokenContract.balanceOf(account);
+      this.state.balance = balance.toNumber() / 10 ** 8;
+    },
+    async 'submit'(e) {
+      e.preventDefault();
+      const form = e.currentTarget;
+      const user = Meteor.user();
+      await TokenContract.transferWithMetadata(
+        Meteor.settings.public.Ethereum.depositAddress,
+        form['amount'].value * 10 ** 8,
+        ethers.utils.formatBytes32String(user.address),
+      );
     },
   },
   private: {
